4ed2d96fe4d32c070df7cad2ff0ed7a131e020b6
Pradeepto Sarkar <psrcofficial10@gmail.com>
2023-10-10 15:12:23 +0530

    community partner form added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,6 +15,10 @@ const Navbar = () => {
     router.push('/Contact');
   };
 
+  const openCPRegistration = () => {
+    router.push('/CPRegister');
+  };
+
   const openRegistration = () => {
     router.push('/Register');
   };
@@ -88,7 +92,7 @@ const Navbar = () => {
             initial={{ opacity: 0, scale: 0.6 }}
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0.6 }}
-            className="w-[200px] bg-gray-50 shadow-xl rounded-lg flex flex-col overflow-hidden absolute top-12 right-5 z-20 "
+            className="w-[300px] bg-gray-50 shadow-xl rounded-lg flex flex-col overflow-hidden absolute top-12 right-5 z-20 "
           >
             <div className="flex flex-col items-center cursor-pointer">
               <p
@@ -124,7 +128,15 @@ const Navbar = () => {
                 onClick={openRegistration}
                 className="m-2 px-4 py-2 rounded-md shadow-md  flex items-center justify-center bg-gray-200 gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
               >
-                CA Registration
+                Campus Ambassadors
+                <MdOutlineAppRegistration />
+              </p>
+
+              <p
+                onClick={openCPRegistration}
+                className="m-2 px-4 py-2 rounded-md shadow-md  flex items-center justify-center bg-gray-200 gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+              >
+                Community Partners
                 <MdOutlineAppRegistration />
               </p>
             </div>
