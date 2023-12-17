a0a8cb82811779b0d45ca48bd8fb4f5bf32d7b3a
Pradeepto Sarkar <psrcofficial10@gmail.com>
2023-12-18 00:13:00 +0530

    mentor component added

---
+++ components/mentors.jsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { footerVariants } from "../utils/motion";
+
+const mentorRegistration = () => (
+    <motion.footer
+      variants={footerVariants}
+      initial="hidden"
+      whileInView="show"
+      className={`${styles.xPaddings}  relative`}
+    >
+      <div className={`${styles.innerWidth} mx-auto flex flex-col gap-3`}>
+        <div className="flex items-center justify-between flex-wrap gap-5">
+          <h4 className="font-bold md:text-[64px] text-[44px] text-white">
+            Call for Mentors
+          </h4>
+        </div>
+      </div>
+  
+      <div style={{ marginTop: "50px" }}>
+        <iframe
+          title="Unique Title for Accessibility"
+          src="https://lu.ma/embed-checkout/evt-cPIsbjh5uMZ1Vx6"
+          width="100%"
+          height="800"
+          style={{
+            border: "1px solid #bfcbda88",
+            borderRadius: "24px",
+            maxWidth: "", // Set maximum width if needed
+            margin: "0 auto", // Center the iframe
+            boxShadow: "10px 10px 20px rgba(0, 0, 0, 0.9)", // Example box shadow
+          }}
+          allowfullscreen=""
+          aria-hidden="false"
+        />
+      </div>
+    </motion.footer>
+  );
+  
+  export default mentorRegistration;
---
+++ pages/mentors.js
@@ -0,0 +1,12 @@
+import { mentorRegistration, Navbar, Footer } from '../components';
+
+const mentorregister = () => (
+  <div className="bg-primary-black overflow-hidden relative">
+    <Navbar />
+    <div className="gradient-05 " />
+    <mentorRegistration />
+    <Footer />
+  </div>
+);
+
+export default mentorregister;
