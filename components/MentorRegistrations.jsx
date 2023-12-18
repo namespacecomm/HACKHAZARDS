51e31045176351f6f74656b5ce00ce2ba118d131
Pradeepto Sarkar <psrcofficial10@gmail.com>
2023-12-18 15:44:29 +0530

    mentors

--- components/MentorRegistrations.jsx
+++ components/MentorRegistrations.jsx
@@ -14,7 +14,7 @@ const MentorRegistration = () => (
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-3`}>
       <div className="flex items-center justify-between flex-wrap gap-5">
         <h4 className="font-bold md:text-[64px] text-[44px] text-white mt-[100px]">
-          Call for Mentors
+          Mentors Registration
         </h4>
       </div>
     </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -251,7 +251,7 @@ const Navbar = () => {
                     onClick={openMentorRegistration}
                     className="m-2 px-4 py-2 rounded-md shadow-md  flex items-center justify-center bg-gray-200 gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
                   >
-                    Call for Mentors
+                    Mentors
                     <MdOutlineAppRegistration />
                   </p>
                 </div>
