f92fb20b461294e9cd8f4192626cb1f1820b6427
Aakash Gupta <mynameaakash46@gmail.com>
2024-01-15 17:02:41 +0530

    added backlinks  updated dates

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -232,7 +232,13 @@ const Navbar = () => {
                   >
                     Contact Us <IoMdContacts />
                   </p>
-
+                  <p
+                    onClick={openMentorRegistration}
+                    className="m-2 px-4 py-2 rounded-md shadow-md  flex items-center justify-center bg-gray-200 gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+                  >
+                    Mentors
+                    <MdOutlineAppRegistration />
+                  </p>
                   <p
                     onClick={openCPRegistration}
                     className="m-2 px-4 py-2 rounded-md shadow-md  flex items-center justify-center bg-gray-200 gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
@@ -247,13 +253,7 @@ const Navbar = () => {
                     Hackathon Evangelists
                     <MdOutlineAppRegistration />
                   </p>
-                  <p
-                    onClick={openMentorRegistration}
-                    className="m-2 px-4 py-2 rounded-md shadow-md  flex items-center justify-center bg-gray-200 gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
-                  >
-                    Mentors
-                    <MdOutlineAppRegistration />
-                  </p>
+
                 </div>
               </motion.div>
             )}
