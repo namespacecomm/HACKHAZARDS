c0071f8289f74b4c5f99c6dfe89e9158bdfc7454
Aakash Gupta <mynameaakash46@gmail.com>
2024-02-13 22:14:14 +0530

    css battle

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -299,14 +299,14 @@ const Feedback = () => (
             />
           </Link>
         </div>
-        {/* <div className="flex flex-col items-center z-[1] mt-[5px] w-[250px]">
+        <div className="flex flex-col items-center z-[1] mt-[5px] w-[250px]">
           <Link
             href="https://cssbattle.dev/"
             target="_blank"
           >
             <img src="29.png" alt="css battle" className="rounded-[15px]" />
           </Link>
-        </div> */}
+        </div>
         {/* <div className="flex flex-col items-center z-[1] mt-[5px] w-[250px]">
           <Link href="https://push.org/" target="_blank">
             <img
